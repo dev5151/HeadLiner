f86a7f56a6860313ca279c844caf4078f23c01a7
dev5151 <iamdev5151@gmail.com>
2020-04-03 12:04:40 +0530

    Routes Added

--- API FILES/rest_api.py
+++ API FILES/rest_api.py
@@ -15,6 +15,8 @@ negativeNews=[]
 newPositiveNews=[]
 neutralNews=[]
 totalLength=0
+newNegativeNews=[]
+newNeutralNews=[]
 
 @app.route("/")
 
@@ -25,15 +27,79 @@ def fetchNews():
     response = requests.get(news_api_url)
 
     data=response.json()
-
-
+    
     return (data)
     
-
 
 @app.route("/positive",methods=['GET'])
 
 def fetchPositiveNews():
+
+    news_api_url = ('http://newsapi.org/v2/top-headlines?''country=in&''apiKey=6efd3ee1e13b448a8e1c8b4c45ec0bc7')
+    
+    response = requests.get(news_api_url)
+
+    data=response.json()
+
+    totalLength=(len(data["articles"]))
+
+    for i in range (0,totalLength):
+        content = data["articles"][i]["content"]
+        if not content :
+            list.append(i)
+        else:
+            blob=TextBlob(content)
+            contentList.append((i,blob.sentiment[0]))
+        
+    
+    print(contentList) 
+    print(list)
+
+    for i in contentList:
+        if 0<i[1]<=1:
+            positiveNews.append(i[0])
+
+    for i in range (0,totalLength):
+        if i in positiveNews:
+            newPositiveNews.append(data["articles"][i])
+
+    return jsonify(newPositiveNews)
+
+@app.route("/negative",methods=['GET'])
+
+def fetchNegativeNews():
+
+    news_api_url = ('http://newsapi.org/v2/top-headlines?''country=in&''apiKey=6efd3ee1e13b448a8e1c8b4c45ec0bc7')
+    
+    response = requests.get(news_api_url)
+
+    data=response.json()
+
+    print (json.dumps(data,indent=3))
+
+    totalLength=(len(data["articles"]))
+
+    for i in range (0,totalLength):
+        content = data["articles"][i]["content"]
+        if not content :
+            list.append(i)
+        else:
+            blob=TextBlob(content)
+            contentList.append((i,blob.sentiment[0]))
+    
+    for i in contentList:
+        if -1<=i[1]<0:
+            negativeNews.append(i[0])
+        
+    for i in range (0,totalLength):
+        if i in negativeNews:
+            newNegativeNews.append(data["articles"][i])
+
+    return jsonify(negativeNews)
+
+@app.route("/neutral",methods=['GET'])
+
+def fetchNeutraleNews():
 
     news_api_url = ('http://newsapi.org/v2/top-headlines?''country=in&''apiKey=6efd3ee1e13b448a8e1c8b4c45ec0bc7')
     
@@ -52,25 +118,16 @@ def fetchPositiveNews():
         else:
             blob=TextBlob(content)
             contentList.append((i,blob.sentiment[0]))
-        
-    
-    print(contentList) 
-    print(list)
 
     for i in contentList:
-        if 0<i[1]<=1:
-            positiveNews.append(i[0])
-        elif -1<=i[1]<0:
-            negativeNews.append(i[0])
-        else:
+        if i[1]==0:
             neutralNews.append(i[0])
 
     for i in range (0,totalLength):
-        if i in positiveNews:
-            newPositiveNews.append(data["articles"][i])
+        if i in neutralNews:
+            newNeutralNews.append(data["articles"][i])
 
-    return jsonify(newPositiveNews)
-
+    return jsonify(newNeutralNews)
 
 if __name__=='__main__':
     app.run(port=5000,debug=True)   
